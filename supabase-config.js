bfd6638d58ebb1b2c3f75256aeee9849c024cbc4
Tsumami Shisho <96518086+Tsumami-Shisho@users.noreply.github.com>
2026-04-01 17:14:20 +0900

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 // ★★★ ここにSupabaseの情報を入れてください ★★★
 // Supabaseダッシュボード → Settings → API から取得
 
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY_HERE';
+const SUPABASE_URL = 'https://lvdoaekdwocierlzjmsu.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2ZG9hZWtkd29jaWVybHpqbXN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMjM3MzUsImV4cCI6MjA5MDU5OTczNX0.7FpIGeLn5JGXlgWtfLwIuGKdMgaVC9VSGda2NONCd0Y';
