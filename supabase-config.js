ef830bf457cae40ae05217ee5e335a4177a8abcf
Tsumami Shisho <96518086+Tsumami-Shisho@users.noreply.github.com>
2026-04-01 18:46:49 +0900

    toriaezu

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-// ★★★ ここにSupabaseの情報を入れてください ★★★
-// Supabaseダッシュボード → Settings → API から取得
-
-const SUPABASE_URL = 'https://lvdoaekdwocierlzjmsu.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2ZG9hZWtkd29jaWVybHpqbXN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMjM3MzUsImV4cCI6MjA5MDU5OTczNX0.7FpIGeLn5JGXlgWtfLwIuGKdMgaVC9VSGda2NONCd0Y';
